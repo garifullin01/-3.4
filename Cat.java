7e8e5c3e2f5343620663d4426fb1ca3fa69d9175
garifullin01 <garifullin_91@mail.ru>
2020-03-21 17:42:02 +0300

    создал первый репозиторий

---
+++ Cat.java
@@ -0,0 +1,89 @@
+
+public class Cat
+{
+    private double originWeight;
+    private double weight;
+
+    private double minWeight;
+    private double maxWeight;
+    private double massOfFood;
+    private double toilet;
+    private double getFat;
+    private static int count;
+
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+        massOfFood = weight / 10;
+        toilet = weight / 17;
+        count = count + 1;
+
+    }
+    public static int getCount()
+    {
+        return count;
+    }
+
+    public Double getFat()
+    {
+        getFat = massOfFood - toilet;
+        return getFat;
+    }
+
+    public Double getToilet()
+    {
+        weight = weight - toilet;
+        return toilet;
+    }
+
+    public Double massOfFood()
+    {
+        weight = weight + massOfFood;
+        return massOfFood;
+    }
+
+    public void meow()
+    {
+        weight = weight - 1;
+        System.out.println("Meow");
+    }
+
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+        if(weight > maxWeight){
+            count = count - 1;
+        }
+
+    }
+
+    public void drink(Double amount)
+    {
+      System.out.println(weight = weight + amount);
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight) {
+            return "Dead";
+        }
+        else if(weight > maxWeight) {
+            return "Exploded";
+        }
+        else if(weight > originWeight) {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+}
---
+++ Loader.java
@@ -0,0 +1,43 @@
+
+public class Loader {
+  public static void main(String[] args)
+
+  {
+    Cat jora = new Cat();
+    System.out.println(Cat.getCount());
+    Cat vacya = new Cat();
+    vacya.feed(700.);
+    System.out.println(vacya.getWeight());
+    vacya.feed(900.);
+    System.out.println(vacya.getWeight());
+    vacya.feed(900.);
+    System.out.println(vacya.getWeight());
+    vacya.feed(900.);
+    System.out.println(vacya.getWeight());
+    vacya.feed(900.);
+    System.out.println(vacya.getWeight());
+    vacya.feed(900.);
+    System.out.println(vacya.getWeight());
+    vacya.feed(900.);
+
+    System.out.println(vacya.getWeight());
+    System.out.println(Cat.getCount());
+
+    Cat murka = new Cat();
+    murka.feed(700.);
+    System.out.println(Cat.getCount());
+
+    Cat cisa = new Cat();
+    cisa.feed(800.);
+    System.out.println(Cat.getCount());
+
+    Cat djeci = new Cat();
+    djeci.feed(900.);
+    System.out.println(Cat.getCount());
+
+    Cat j = new Cat();
+    j.feed(900.);
+    System.out.println(Cat.getCount());
+  }
+
+}
